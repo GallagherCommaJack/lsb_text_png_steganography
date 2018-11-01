af242083e6c8472aa11dcdfe469799111fb42a95
Peter Heesterman <heesterman.peter@gmail.com>
2018-11-01 16:57:39 +1300

    fix(hider): is_payload_too_large method to account for header length

--- src/hider/mod.rs
+++ src/hider/mod.rs
@@ -3,7 +3,7 @@ pub fn is_payload_too_large(payload_length: u32, image_x_max: u32, image_y_max: 
     let pixels = image_x_max * image_y_max;
     let pixels_to_hold_a_byte = 3_u32;
 
-    (payload_length * pixels_to_hold_a_byte) > pixels
+    ((payload_length + 4) * pixels_to_hold_a_byte) > pixels
 }
 
 #[cfg(test)]
@@ -12,16 +12,16 @@ mod tests {
 
     #[test]
     fn tiny_payload_case() {
-        assert!(!is_payload_too_large(1, 2, 2))
+        assert!(!is_payload_too_large(1, 3, 5))
     }
 
     #[test]
     fn tiny_payload_case_fail() {
-        assert!(is_payload_too_large(2, 2, 2))
+        assert!(is_payload_too_large(2, 3, 5))
     }
 
     #[test]
     fn perfect_payload() {
-        assert!(!is_payload_too_large(3, 3, 3))
+        assert!(!is_payload_too_large(100, 3, 104))
     }
 }
